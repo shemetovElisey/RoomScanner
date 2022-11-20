a8152b505f94360873888c6a7d46f629d71a4e37
Elisey Shemetov <shemetov.elisey@gmail.com>
2022-11-20 16:08:37 +0700

    first commit

--- RoomScanner/AppDelegate.swift
+++ RoomScanner/AppDelegate.swift
@@ -6,31 +6,33 @@
 //
 
 import UIKit
+import RoomPlan
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    private enum ConfigurationType: String {
+        case hasLidar = "Default Configuration"
+        case unsupported = "Unsupported Device"
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let configurationName = RoomCaptureSession.isSupported ?
+            ConfigurationType.hasLidar.rawValue : ConfigurationType.unsupported.rawValue
+        
+        return UISceneConfiguration(name: configurationName, sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
